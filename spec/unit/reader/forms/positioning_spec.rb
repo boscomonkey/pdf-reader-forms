92162a3d81f305069a004310794a7e077eecdc43
Casey Kuhlman <caseykuhlman@watershedlegal.com>
2013-08-01 15:26:19 +0300

    Move fuzzed y to positioning module

--- spec/unit/reader/forms/positioning_spec.rb
+++ spec/unit/reader/forms/positioning_spec.rb
@@ -4,9 +4,10 @@ describe PDF::Reader::Forms::Positioning do
   let(:forms_reader) { PDF::Reader::Forms.new(source,options) }
   let(:source) { nil } # we're just going to mock the PDF source here
   let(:options) { {} }
+  let(:page) { 1 }
 
   context 'with Mocked Resources' do
-    let(:page) { 1 }
+
     before do
       forms_reader.should_receive(:load_content).with(page).and_return(given_page_content)
     end
@@ -134,6 +135,84 @@ describe PDF::Reader::Forms::Positioning do
           let(:expected_position) { test_expectations[:expected_position] }
           subject { forms_reader.text_position(match_term,page) }
           it { should eql(expected_position) }
+        end
+      end
+    end
+  end
+
+
+  describe "should Find the Content and Positions Correctly" do
+    context "with Mocked Resources" do
+      let(:source) { nil }
+      before do
+        forms_reader.should_receive(:load_content).with(page).and_return(given_page_content)
+      end
+
+      {
+        :with_simple_text => {
+          :source_page_content => {10.0=>{10.0=>[160.0,"a first bit of text"]}},
+          :expected_precise_content => {10.0=>{10.0=>[160.0,"a first bit of text"]}},
+          :expected_fuzzed_content => [[10.0,[[10.0,160.0,"a first bit of text"]]]]
+        },
+        :with_widely_separated_text => {
+          :source_page_content => {20.0=>{10.0=>[160.0,"a first bit of text"]},10.0=>{20.0=>[175.0,"a second bit of text"]}},
+          :expected_precise_content => {20.0=>{10.0=>[160.0,"a first bit of text"]},10.0=>{20.0=>[175.0,"a second bit of text"]}},
+          :expected_fuzzed_content => [[20.0, [[10.0,160.0,"a first bit of text"]]], [10.0, [[20.0,175.0,"a second bit of text"]]]]
+        },
+        :with_unsorted_y_text => {
+          :source_page_content => {10.0=>{10.0=>[160.0,"a first bit of text"]},20.0=>{20.0=>[175.0,"a second bit of text"]}},
+          :expected_precise_content => {10.0=>{10.0=>[160.0,"a first bit of text"]},20.0=>{20.0=>[175.0,"a second bit of text"]}},
+          :expected_fuzzed_content => [[20.0, [[20.0, 175.0, "a second bit of text"]]], [10.0, [[10.0, 160.0, "a first bit of text"]]]]
+        },
+        :with_fuzzed_y_text => {
+          :source_page_content => {20.0=>{10.0=>[160.0,"a first bit of text"]},18.0=>{12.0=>[150.0,"a second bit of text"]}},
+          :expected_precise_content => {20.0=>{10.0=>[160.0,"a first bit of text"]},18.0=>{12.0=>[150.0,"a second bit of text"]}},
+          :expected_fuzzed_content => [[20.0, [[10.0, 160.0, "a first bit of text"], [12.0, 150.0, "a second bit of text"]]]]
+        },
+        :with_widely_separated_fuzzed_y_text => {
+          :y_precision => 25,
+          :source_page_content => {20.0=>{10.0=>[160.0,"a first bit of text"]},10.0=>{20.0=>[175.0,"a second bit of text"]}},
+          :expected_precise_content => {20.0=>{10.0=>[160.0,"a first bit of text"]},10.0=>{20.0=>[175.0,"a second bit of text"]}},
+          :expected_fuzzed_content => [[20.0, [[10.0, 160.0, "a first bit of text"], [20.0, 175.0, "a second bit of text"]]]]
+        },
+        :with_multiple_row_text => {
+          :source_page_content => {10.0=>{10.0=>[50.0,"first"]},8.0=>{60.0=>[100.0,"second"],150.0=>[190.0,"third"]}},
+          :expected_precise_content => {10.0=>{10.0=>[50.0,"first"]},8.0=>{60.0=>[100.0,"second"],150.0=>[190.0,"third"]}},
+          :expected_fuzzed_content => [[10.0, [[10.0, 50.0, "first"], [60.0, 100.0, "second"], [150.0, 190.0, "third"]]]]
+        },
+        :with_unsorted_x_text => {
+          :source_page_content => {10.0=>{200.0=>[240.0,"fourth"],150.0=>[190.0,"third"],10.0=>[50.0,"first"],60.0=>[90.0,"second"]}},
+          :expected_precise_content => {10.0=>{200.0=>[240.0,"fourth"],150.0=>[190.0,"third"],10.0=>[50.0,"first"],60.0=>[90.0,"second"]}},
+          :expected_fuzzed_content => [[10.0, [[10.0,50.0,"first"], [60.0,90.0,"second"], [150.0, 190.0, "third"], [200.0, 240.0, "fourth"]]]]
+        }
+      }.each do |test_name,test_expectations|
+        context test_name do
+          let(:given_page_content) { test_expectations[:source_page_content] }
+          let(:options) {
+            if (y_precision = test_expectations[:y_precision]) && y_precision != :default
+              { :y_precision => y_precision }
+            else
+              {}
+            end
+          }
+          describe "#content for Mocked Resources" do
+            subject { forms_reader.content(page) }
+            it { should eql(test_expectations[:expected_fuzzed_content]) }
+          end
+          describe "#precise_content for Mocked Resources" do
+            subject { forms_reader.precise_content(page) }
+            it { should eql(test_expectations[:expected_precise_content]) }
+          end
+        end
+      end
+
+      context "with PDF Forms" do
+        pdf_forms_blocks_expectations.each do |forms_file,expectations|
+          context "#precise_content for #{forms_file}" do
+            let(:source) { pdf_forms(forms_file) }
+            subject { forms_reader.content(1) }
+            it { should eql(expectations[:test_precise_content])}
+          end
         end
       end
     end
